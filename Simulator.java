326a8aca15dd09180ba71d05b2b1bb28e80a5ac4
Kefir101 <82855176+Kefir101@users.noreply.github.com>
2021-10-04 11:44:05 -0700

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -1,8 +1,11 @@
 import java.util.Arrays;
 
 public class Simulator {
-    private static double fireSpreadHorizontalChance = 0.8;
-    private static double fireSpreadDiagonalChance = 0.6;
+    private static final int NO_TREE = 0;
+    private static final int LIVING_TREE = 1;
+    private static final int BURNING_TREE = 2;
+    private static final double fireSpreadHorizontalChance = 0.8;
+    private static final double fireSpreadDiagonalChance = 0.57;
     private static final int hourBurnThreshold = 5;
     double treeDensity;
     int[][] forest;
@@ -30,7 +33,7 @@ public class Simulator {
         double totalSize = getHeight() * getWidth();
         for (int i = 0; i < getHeight(); i++) {
             for (int j = 0; j < getWidth(); j++) {
-                if(forest[i][j] == 2) burnedTrees++;
+                if(forest[i][j] == BURNING_TREE) burnedTrees++;
             }
         }
         return burnedTrees/(totalSize*treeDensity);
@@ -40,7 +43,7 @@ public class Simulator {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (Math.random() < treeDensity) {
-                    forest[i][j] = 1;
+                    forest[i][j] = LIVING_TREE;
                 }
             }
         }
@@ -51,8 +54,8 @@ public class Simulator {
         while(!setFireToTree){
             int row = (int)(Math.random()*getHeight());
             int col = (int)(Math.random()*getWidth());
-            if(isInBound(row, col) && forest[row][col] == 1){
-                forest[row][col] = 2;
+            if(isInBound(row, col) && forest[row][col] == LIVING_TREE){
+                forest[row][col] = BURNING_TREE;
                 setFireToTree = true;
             }
         }
@@ -62,40 +65,44 @@ public class Simulator {
         int [][] forestClone = Arrays.stream(forest).map(int[]::clone).toArray(int[][]::new);
         for (int i = 0; i < getHeight(); i++) {
             for (int j = 0; j < getWidth(); j++) {
-                if(forestClone[i][j] == 2){
-                    for (int k = i-1; k <= i+1; k++) {
-                        for (int l = j-1; l <= j+1; l++) {
-                            if(isInBound(k, l) && forestClone[k][l] == 1){ //is in bounds and is tree
-                                int indexDiff = Math.abs(i - k) + Math.abs(j - l);
-                                if(indexDiff == 1){ //horizontal tree
-                                    if(this.wind < 0 && l - j < 0 || this.wind > 0 && l - j > 0){
-                                        fireSpreadHorizontalChance *= (1 + Math.abs(wind));
-                                    }else{
-                                        fireSpreadHorizontalChance /= (1 + Math.abs(wind));
-                                    }
-                                    double fireChance = Math.random();
-                                    if(fireChance < fireSpreadHorizontalChance){
-                                        forest[k][l] = 2;
-                                        treesBurned++;
-                                    }
-                                }
-                                if(indexDiff == 2){ //diagonal tree
-                                    double fireChance = Math.random();
-                                    if(fireChance < fireSpreadDiagonalChance) {
-                                        forest[k][l] = 2;
-                                        treesBurned++;
-                                    }
-                                }
-                            }
+                if(forestClone[i][j] == BURNING_TREE){
+                    treesBurned += burnNearbyTrees(i, j, forestClone);
+                }
+            }
+        }
+        return treesBurned > 0;
+    }
+    private int burnNearbyTrees(int i, int j, int[][] forestClone) {
+        int treesBurned = 0;
+        for (int k = i-1; k <= i+1; k++) {
+            for (int l = j-1; l <= j+1; l++) {
+                if(isInBound(k, l) && forestClone[k][l] == LIVING_TREE){
+                    int indexDiff = Math.abs(i - k) + Math.abs(j - l);
+                    double tempHorzChance = fireSpreadHorizontalChance;
+                    double tempDiagChance = fireSpreadDiagonalChance;
+                    if(indexDiff == 1){ //horizontal tree
+                        if (this.wind < 0 && l - j < 0 || this.wind > 0 && l - j > 0) {
+                            tempHorzChance *= (1 + Math.abs(wind));
+                        } else {
+                            tempHorzChance /= (1 + Math.abs(wind));
+                        }
+                        double fireChance = Math.random();
+                        if(fireChance < tempHorzChance){
+                            forest[k][l] = BURNING_TREE;
+                            treesBurned++;
+                        }
+                    }
+                    if(indexDiff == 2){ //diagonal tree
+                        double fireChance = Math.random();
+                        if(fireChance < tempDiagChance) {
+                            forest[k][l] = BURNING_TREE;
+                            treesBurned++;
                         }
                     }
                 }
             }
         }
-//        System.out.println("forest: " + Arrays.deepToString(forest));
-//        System.out.println("-------------");
-//        System.out.println("clone: " + Arrays.deepToString(forestClone));
-        return treesBurned > 0;
+        return treesBurned;
     }
     private boolean isInBound(int row, int col){
         return row >= 0 && col >= 0 && row < getHeight() && col < getWidth();
