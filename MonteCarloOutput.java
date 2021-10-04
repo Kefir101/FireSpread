7a01d6067b16bd6b1797462903a672de7ac547c0
Kefir101 <82855176+Kefir101@users.noreply.github.com>
2021-10-04 11:43:44 -0700

    Update MonteCarloOutput.java

--- MonteCarloOutput.java
+++ MonteCarloOutput.java
@@ -3,12 +3,9 @@ public class MonteCarloOutput {
         int forestRows = 100;
         int forestColumns = 100;
         System.out.println("Tree Density   |   Average Hours   |  Percent Burned");
-        for (double treeDensity = 0.01; treeDensity <= 1; treeDensity += 0.01) {
-//            System.out.println("Tree Density: " + ((int)(treeDensity*100)) + "%, Hours: " +
-//                    getAvgHoursForTreeDensity(forestRows, forestColumns, treeDensity));
+        for (double treeDensity = 0.005; treeDensity <= 1; treeDensity += 0.005) {
             double[] info = getBurnInfo(forestRows, forestColumns, treeDensity, 0);
-
-            System.out.println(((int)(treeDensity*100)) + ", " + info[0] + ", " + Math.round(info[1] * 100 * 100.0) / 100.0);
+            System.out.println(Math.round(treeDensity * 1000.0)/10.0 + ", " + Math.round(info[0]*1000.0)/1000.0 + ", " + Math.round(info[1] * 100 * 100.0) / 100.0);
         }
         System.out.println("Wind   |   Average Hours   |  Percent Burned");
         for (double wind = -1; wind <= 1; wind += 0.01) {
@@ -17,7 +14,7 @@ public class MonteCarloOutput {
         }
     }
     private static double[] getBurnInfo(int forestRows, int forestColumns, double treeDensity, double wind) {
-        int experiments = 10;
+        int experiments = 30;
         double[] info = new double[2];
         for (int experiment = 0; experiment < experiments; experiment++) {
             Simulator sim = new Simulator(forestRows, forestColumns, treeDensity, wind);
